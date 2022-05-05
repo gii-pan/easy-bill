3c34c7030487df1840610fcb70b853981c67095c
unknown <giuliaborgesoliveiraa@gmail.com>
2022-05-05 11:34:52 -0300

    Adicionando arquivos do projeto

---
+++ src/main/java/br/com/alura/oobj/easybill/EasyBillApplication.java
@@ -0,0 +1,13 @@
+package br.com.alura.oobj.easybill;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EasyBillApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EasyBillApplication.class, args);
+	}
+
+}
